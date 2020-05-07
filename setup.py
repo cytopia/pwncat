c3ea2f0eb2f5fc9f375b135f708fa4c13bc84244
cytopia <cytopia@everythingcli.org>
2020-05-07 22:52:11 +0200

    Update version to v0.0.12-alpha

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="pwncat",
-    version="0.0.11-alpha",
+    version="0.0.12-alpha",
     description="Netcat on steroids with Firewall and IPS evasion, bind and reverse shell and port-forwarding.",
     license="MIT",
     long_description=long_description,
